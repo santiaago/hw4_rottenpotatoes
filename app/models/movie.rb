870f30cfb669d71b2bbe0b3282c24bc15621987f
Santiaago <santiago.ariassar@gmail.com>
2012-04-09 14:36:35 +0200

    yo

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,21 +3,20 @@ class Movie < ActiveRecord::Base
     %w(G PG PG-13 NC-17 R)
   end
   class Movie::NoDirectorError < StandardError; end
-    def self.similar_movies(id)
-      movie = Movie.find id
-      logger.info "movie=#{movie.inspect}"
-      unless m.director.blank?
-        Movie.find_all_by_director m.director
-      else
-        raise Movie::NoDirectorError, "'#{movie.title}' has no director info"
-      end
+  def self.similar_movies(id)
+    m = Movie.find id
+    logger.info "movie=#{m.inspect}"
+    unless m.director.blank?
+      Movie.find_all_by_director m.director
+    else
+      raise Movie::NoDirectorError, "'#{m.title}' has no director info"
     end
-    def similar_movies
-      if self.director
-        Movie.find_all_by_director self.director
-      else
-        raise Movie::NoDirectorError, "#{self.title} has no director info"
-      end
+  end
+  def similar_movies
+    if self.director
+      Movie.find_all_by_director self.director
+    else
+      raise Movie::NoDirectorError, "#{self.title} has no director info"
     end
   end
 end
